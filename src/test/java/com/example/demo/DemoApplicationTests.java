f9cf1c1a5daa744421ce96a2eaff740e0aee53e1
KoKimss <seungsu8848@ajou.ac.kr>
2024-01-16 18:07:13 +0900

    test : 실패테스트 코드 작성

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,9 +12,10 @@ class DemoApplicationTests {
     void contextLoads() {
     }
 
-    @Test()
+    @Test
     @DisplayName("테스트 실행!")
     void 테스트() {
+        Assertions.assertTrue(false);
         System.out.println("테스트 실행 성공!");
     }
 
