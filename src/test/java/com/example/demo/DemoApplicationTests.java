3adb356e3930a6181044860a88cb3e100d52e0ad
KoKimss <seungsu8848@ajou.ac.kr>
2024-01-16 17:52:54 +0900

    test : 테스트성공 코드 작성

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test()
+    @DisplayName("테스트 실행!")
+    void 테스트() {
+        System.out.println("테스트 실행 성공!");
     }
 
 }
