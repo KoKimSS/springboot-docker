93a73a0396c02926a824ae205c9883d5b1dc3e43
KoKimss <seungsu8848@ajou.ac.kr>
2024-01-18 14:40:59 +0900

    test : 실패테스트 작성

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -19,4 +19,10 @@ class DemoApplicationTests {
         System.out.println("테스트 실행 성공!");
     }
 
+    @Test
+    @DisplayName("실패 테스트 ! ")
+    void 실패테스트() {
+        Assertions.assertTrue(false);
+    }
+
 }
